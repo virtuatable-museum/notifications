143bc0fcb7402d1635e8cf2321c302f19290d82e
Babausse <courtois.vincent@outlook.com>
2019-04-13 17:15:45 +0200

    Add the restart task in a dirty way

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,3 +9,11 @@ set :branch, 'master'
 append :linked_files, 'config/mongoid.yml'
 
 append :linked_dirs, 'bundle'
+
+namespace :deploy do
+  desc 'Start the server'
+  after :finishing, :start do
+    run "cd #{deploy_to};if [ -f tmp/pids/arkaan.pid ] && [ -e /proc/$(cat tmp/pids/arkaan.pid) ]; then kill -9 `cat tmp/pids/arkaan.pid`; fi"
+    run "cd #{deploy_to}; RACK_ENV=production bundle exec rackup -o 0.0.0.0 -p 80 --env production -P tmp/pids/arkaan.pid"
+  end
+end
