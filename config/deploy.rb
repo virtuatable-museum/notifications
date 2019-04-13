593a2105520d4a06bd4125794136d08053b899ba
Babausse <courtois.vincent@outlook.com>
2019-04-13 19:11:03 +0200

    Change the restart task to correctly handle PIDs

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,8 +12,17 @@ append :linked_dirs, 'bundle'
 
 namespace :deploy do
   desc 'Start the server'
-  after :finishing, :start do
-    run "cd #{deploy_to};if [ -f tmp/pids/arkaan.pid ] && [ -e /proc/$(cat tmp/pids/arkaan.pid) ]; then kill -9 `cat tmp/pids/arkaan.pid`; fi"
-    run "cd #{deploy_to}; RACK_ENV=production bundle exec rackup -o 0.0.0.0 -p 80 --env production -P tmp/pids/arkaan.pid"
+  task :start do
+    on roles(:all) do
+      within current_path do
+        if test('[ -f /tmp/arkaan.pid ]')
+          puts 'Le fichier du PID a bien été trouvé et va être supprimé.'
+          execute :kill, '-9 `cat /tmp/arkaan.pid`'
+        else
+          puts "Le fichier du PID n'a pas été trouvé et ne peux pas être supprimé."
+        end
+        execute :bundle, 'exec rackup -p 9292 --env production -o 0.0.0.0 -P /tmp/arkaan.pid'
+      end
+    end
   end
 end
