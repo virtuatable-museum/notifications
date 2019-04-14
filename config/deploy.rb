e073daf53c73c1561c57b6b6b2a538b871eb69ca
Babausse <courtois.vincent@outlook.com>
2019-04-14 13:43:36 +0200

    Move the start task in a dedicated file and try to load variables from a .env file

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,22 +7,6 @@ set :repo_url, 'git@github.com:jdr-tools/notifications.git'
 set :branch, 'master'
 
 append :linked_files, 'config/mongoid.yml'
+append :linked_files, '.env'
 
 append :linked_dirs, 'bundle'
-
-namespace :deploy do
-  desc 'Start the server'
-  after :finishing, :start do
-    on roles(:all) do
-      within current_path do
-        if test('[ -f /tmp/arkaan.pid ]')
-          puts 'Le fichier du PID a bien été trouvé et va être supprimé.'
-          execute :kill, '-9 `cat /tmp/arkaan.pid`'
-        else
-          puts "Le fichier du PID n'a pas été trouvé et ne peux pas être supprimé."
-        end
-        execute :bundle, 'exec rackup -p 9292 --env production -o 0.0.0.0 -P /tmp/arkaan.pid --daemonize'
-      end
-    end
-  end
-end
